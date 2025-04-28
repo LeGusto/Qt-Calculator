fb2913b6ebb445544d3cfedebd726af88d3c65ea
LeGusto <janusonis.gustas@gmail.com>
2025-04-28 17:10:46 +0300

    First commit

---
+++ build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Calculator_FILE /home/LeGusto/Documents/Calculator/build/Desktop-Debug/Calculator)
+set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QPushButton>
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app (argc, argv);
+
+    QPushButton button ("Hello world !");
+    button.show();
+
+    return app.exec();
+}
